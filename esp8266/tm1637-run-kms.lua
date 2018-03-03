118c77dee9f6490560472d6caab907ec309a156b
László ÁSHIN <laszlo@ashin.hu>
2018-03-03 22:30:09 +0100

    Add tm1637 run kms project

---
+++ esp8266/tm1637-run-kms.lua
@@ -0,0 +1,15 @@
+tm1637 = require('tm1637')
+tm1637.init(4, 3)
+tm1637.set_brightness(7)
+m = mqtt.Client(node.chipid(), 120)
+m:on("message", function(client, topic, data)
+	if data ~= nil then
+		tm1637.clear()
+		tm1637.write_string(data)
+	end
+end)
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+	m:connect(T.gateway, 1883, 0, function(client)
+		client:subscribe("kms", 0)
+	end)
+end)
