430bf6258b8e18383579f8799ae1bae66b884e94
László ÁSHIN <laszlo@ashin.hu>
2018-03-04 13:09:43 +0100

    Make use of enduser_setup
    config_mode can be activated through pulling RX to GND

--- esp8266/tm1637-run-kms.lua
+++ esp8266/tm1637-run-kms.lua
@@ -1,8 +1,11 @@
+gpio.mode(9, gpio.INPUT, gpio.PULLUP)
+config_mode = gpio.read(9) == gpio.LOW
+uart.setup(0, 9600, 8, uart.PARITY_NONE, uart.STOPBITS_1)
 tm1637 = require('tm1637')
 tm1637.init(4, 3)
 tm1637.set_brightness(7)
 m = mqtt.Client(node.chipid(), 120)
-m:on("message", function(client, topic, data)
+function handler(client, topic, data)
 	if data == olddata then
 		return
 	end
@@ -11,9 +14,29 @@ m:on("message", function(client, topic, data)
 	if data ~= nil then
 		tm1637.write_string(data)
 	end
-end)
-wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+end
+m:on("message", handler)
+m:on("offline", handler)
+function sub(T)
 	m:connect(T.gateway, 1883, 0, function(client)
 		client:subscribe("kms", 0)
 	end)
-end)
+end
+function boot_with_config()
+	_, _, gw = wifi.sta.getip()
+	if gw ~= nil then
+		sub({ gateway = gw })
+	else
+		wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, sub)
+	end
+end
+if config_mode then
+	wifi.sta.clearconfig()
+	tm1637.write_string("8.8.8.8")
+	enduser_setup.start(function()
+		wifi.sta.autoconnect(1)
+		boot_with_config()
+	end)
+else
+	boot_with_config()
+end
