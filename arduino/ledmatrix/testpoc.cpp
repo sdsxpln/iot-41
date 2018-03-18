5ffd6793322f865e548712bbd9beb29cbea74c48
László ÁSHIN <laszlo@ashin.hu>
2018-03-18 18:59:42 +0100

    Use fixed point angle

--- arduino/ledmatrix/testpoc.cpp
+++ arduino/ledmatrix/testpoc.cpp
@@ -6,23 +6,22 @@
 #include <algorithm>
 
 struct Math {
+	static constexpr uint8_t pi() { return 128; }
+
 	Math() {
-		std::generate(dtab_.begin(), dtab_.end(), [phi = 0.0]() mutable {
-			const auto p = phi;
-			phi += M_PI / 128;
-			return 256.0 * std::sin(p);
+		std::generate(dtab_.begin(), dtab_.end(), [n = 0]() mutable {
+			return 256.0 * std::sin(n++ * M_PI / pi());
 		});
-		std::generate(p0tab_.begin(), p0tab_.end(), [phi = 0.0]() mutable {
-			const auto p = phi;
-			phi += M_PI / 128;
-			return 128.0 + 256.0 * 47.501 * (1.0 + sqrt(2) * sin(p+5*M_PI/4));
+		std::generate(p0tab_.begin(), p0tab_.end(), [n = 0]() mutable {
+			const double phi = n++ * M_PI / pi();
+			return 128.0 + 256.0 * 47.501 * (1.0 + sqrt(2) * sin(phi+5*M_PI/4));
 		});
 	}
 
-	int16_t u0(double phi) const { return p0tab_[uint8_t(64.0 + 128.0 * phi / M_PI)]; }
-	int16_t v0(double phi) const { return p0tab_[uint8_t(128.0 * phi / M_PI)]; }
-	int16_t du(double phi) const { return dtab_[uint8_t(64.0 + 128.0 * phi / M_PI)]; }
-	int16_t dv(double phi) const { return dtab_[uint8_t(128.0 * phi / M_PI)]; }
+	int16_t u0(uint8_t phi) const { return p0tab_[(pi() / 2 + phi) & 255]; }
+	int16_t v0(uint8_t phi) const { return p0tab_[phi]; }
+	int16_t du(uint8_t phi) const { return dtab_[(pi() / 2 + phi) & 255]; }
+	int16_t dv(uint8_t phi) const { return dtab_[phi]; }
 
 private:
 	std::array<int16_t, 256> dtab_;
@@ -33,23 +32,23 @@ TEST_GROUP(Math) {};
 
 TEST(Math, Rotation) {
 	static const struct {
-		double phi;
+		uint8_t phi;
 		int16_t u0;
 		int16_t v0;
 		int16_t du;
 		int16_t dv;
 	} vectors[] = {
-		  { 0.0 * M_PI / 2.0,  0 * 256 + 127,  0 * 256 + 128,  256,    0 }
-		, { 1.0 * M_PI / 2.0, 95 * 256 + 128,  0 * 256 + 127,    0,  256 }
-		, { 2.0 * M_PI / 2.0, 95 * 256 + 128, 95 * 256 + 128, -256,    0 }
-		, { 3.0 * M_PI / 2.0,  0 * 256 + 128, 95 * 256 + 128,    0, -256 }
+		  { 0 * Math::pi() / 2,  0 * 256 + 127,  0 * 256 + 128,  256,    0 }
+		, { 1 * Math::pi() / 2, 95 * 256 + 128,  0 * 256 + 127,    0,  256 }
+		, { 2 * Math::pi() / 2, 95 * 256 + 128, 95 * 256 + 128, -256,    0 }
+		, { 3 * Math::pi() / 2,  0 * 256 + 128, 95 * 256 + 128,    0, -256 }
 	};
 
 	const Math m;
 
 	for (const auto& v : vectors) {
 		std::ostringstream oss;
-		oss << "phi=" << v.phi;
+		oss << "phi=" << int(v.phi);
 		const auto text = oss.str();
 		CHECK_EQUAL_TEXT(v.u0, m.u0(v.phi), text.c_str());
 		CHECK_EQUAL_TEXT(v.v0, m.v0(v.phi), text.c_str());
