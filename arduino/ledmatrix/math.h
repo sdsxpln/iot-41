0b121925b8c8e3590bfe42eaa1c946998f87e753
László ÁSHIN <laszlo@ashin.hu>
2018-03-20 23:38:10 +0100

    Name lookup functions properly

--- arduino/ledmatrix/math.h
+++ arduino/ledmatrix/math.h
@@ -17,12 +17,14 @@ struct Table {
 		std::generate(tab_.begin(), tab_.end(), [g, n = 0]() mutable { return g(std::sin(n++ * M_PI / pi())); });
 	}
 
-	int16_t lookup(uint8_t phi) const {
-		return (phi < 128) ? lookup2(phi) : (mid_ - lookup2(phi - 128));
+	int16_t cos(uint8_t phi) const { return sin(phi + Table::pi() / 2); }
+
+	int16_t sin(uint8_t phi) const {
+		return (phi < 128) ? lookup(phi) : (mid_ - lookup(phi - 128));
 	}
 
 private:
-	int16_t lookup2(uint8_t phi) const {
+	int16_t lookup(uint8_t phi) const {
 		assert(phi < 128);
 		return (phi <= 64) ? tab_[phi] : tab_[128 - phi];
 	}
@@ -38,10 +40,10 @@ struct Math {
 	{
 	}
 
-	int16_t u0(uint8_t phi) const { return v0(phi + Table::pi() / 2); }
-	int16_t v0(uint8_t phi) const { return p0tab_.lookup(phi + Table::pi() / 4 * 5); }
-	int16_t du(uint8_t phi) const { return dv(phi + Table::pi() / 2); }
-	int16_t dv(uint8_t phi) const { return dtab_.lookup(phi); }
+	int16_t u0(uint8_t phi) const { return p0tab_.cos(phi + Table::pi() / 4 * 5); }
+	int16_t v0(uint8_t phi) const { return p0tab_.sin(phi + Table::pi() / 4 * 5); }
+	int16_t du(uint8_t phi) const { return dtab_.cos(phi); }
+	int16_t dv(uint8_t phi) const { return dtab_.sin(phi); }
 
 private:
 	Table dtab_;
